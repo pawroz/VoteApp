a5779bcecca66628edb4f2bba3922cb6fb539794
pawroz <rozplochowskipawel9@gmail.com>
2021-03-22 17:10:03 +0100

    landing page stored in other app

--- VoteProject/VoteProject/urls.py
+++ VoteProject/VoteProject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+	path('', include('LandingPage.urls')),
 	path('VoteApp/', include('VoteApp.urls')),
     path('admin/', admin.site.urls),
 
