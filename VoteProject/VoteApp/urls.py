13b355afbb3c0c5603cdf44c278c8ce406990fa2
pawroz <rozplochowskipawel9@gmail.com>
2021-03-24 13:36:46 +0100

    get the data of specific questions votes-jquery

--- VoteProject/VoteApp/urls.py
+++ VoteProject/VoteApp/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'VoteApp'
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('detail/<str:pk>', views.detail, name='detail'),
-	path('results/<str:pk>', views.results, name='results'),
-	path('vote/<str:pk>', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('detail/<str:pk>', views.detail, name='detail'),
+    path('results/<str:pk>', views.results, name='results'),
+    path('vote/<str:pk>', views.vote, name='vote'),
+    path('resultsData/<str:pk>', views.resultsData, name='resultsData'),
 
 ]
--- VoteProject/VoteApp/views.py
+++ VoteProject/VoteApp/views.py
@@ -1,61 +1,71 @@
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
-
+from django.http import JsonResponse
 from .models import *
 
 
 def index(request):
-	latest_questions = Question.objects.order_by('-pub_date')[:5]
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
 
-
-	context = {'latest_questions':latest_questions}
-	return render(request,'VoteApp/index.html', context)
+    context = {'latest_questions': latest_questions}
+    return render(request, 'VoteApp/index.html', context)
 
 
 def detail(request, pk):
-	try:
-		specific_question = Question.objects.get(pk=pk)
+    try:
+        specific_question = Question.objects.get(pk=pk)
 
-	except Question.DoesNotExist:
-		raise Http404('Ankieta nie istnieje')
+    except Question.DoesNotExist:
+        raise Http404('Ankieta nie istnieje')
 
-
-	context = {'specific_question':specific_question}
-	return render(request, 'VoteApp/detail.html', context)
+    context = {'specific_question': specific_question}
+    return render(request, 'VoteApp/detail.html', context)
 
 
 def results(request, pk):
-	try:
-		specific_question = Question.objects.get(pk=pk)
-		print(specific_question)
-	except Question.DoesNotExist:
-		raise Http404('Ankieta nie istnieje')
+    try:
+        specific_question = Question.objects.get(pk=pk)
+        print(specific_question)
+    except Question.DoesNotExist:
+        raise Http404('Ankieta nie istnieje')
 
-	context = {'specific_question':specific_question}
-	return render(request, 'VoteApp/results.html', context)
+    context = {'specific_question': specific_question}
+    return render(request, 'VoteApp/results.html', context)
 
 
 def vote(request, pk):
-	print(request.POST['choice'])
-	try:
-		specific_question = Question.objects.get(pk=pk)
-	except Question.DoesNotExist:
-		raise Http404('Ankieta nie istnieje')
+    print(request.POST['choice'])
+    try:
+        specific_question = Question.objects.get(pk=pk)
+    except Question.DoesNotExist:
+        raise Http404('Ankieta nie istnieje')
 
-	try:
-		selected_choice = specific_question.choice_set.get(pk=request.POST['choice'])
-	except (Choice.DoesNotExist):
-		# Redisplay the question voting form.
-		return render(request, 'VoteApp/detail.html', {
-			'specific_question': specific_question,
-			'error_message': "You didn't select a choice.",
-		})
-	
-	selected_choice.votes += 1
-	selected_choice.save()
-	# Always return an HttpResponseRedirect after successfully dealing
-	# with POST data. This prevents data from being posted twice if a
-	# user hits the Back button.
-	return HttpResponseRedirect(reverse('VoteApp:results', args=(specific_question.id)))
+    try:
+        selected_choice = specific_question.choice_set.get(
+            pk=request.POST['choice'])
+    except (Choice.DoesNotExist):
+
+        return render(request, 'VoteApp/detail.html', {
+            'specific_question': specific_question,
+            'error_message': "You didn't select a choice.",
+        })
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse('VoteApp:results', args=(specific_question.id,)))
+
+
+def resultsData(request, pk):
+    voteData = []
+
+    question = Question.objects.get(id=pk)
+    votes = question.choice_set.all()
+
+    for i in votes:
+        voteData.append({i.choice: i.votes})
+
+    print(voteData)
+    return JsonResponse(voteData, safe=False)
