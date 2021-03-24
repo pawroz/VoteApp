9a538b98fc2a845e540426b77d19f61d47b02f18
pawroz <rozplochowskipawel9@gmail.com>
2021-03-24 12:15:44 +0100

    display first empty char

--- VoteProject/VoteApp/views.py
+++ VoteProject/VoteApp/views.py
@@ -46,16 +46,16 @@ def vote(request, pk):
 
 	try:
 		selected_choice = specific_question.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
+	except (Choice.DoesNotExist):
 		# Redisplay the question voting form.
 		return render(request, 'VoteApp/detail.html', {
 			'specific_question': specific_question,
 			'error_message': "You didn't select a choice.",
 		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		# Always return an HttpResponseRedirect after successfully dealing
-		# with POST data. This prevents data from being posted twice if a
-		# user hits the Back button.
-		return HttpResponseRedirect(reverse('VoteApp:results', args=(specific_question.id,)))
+	
+	selected_choice.votes += 1
+	selected_choice.save()
+	# Always return an HttpResponseRedirect after successfully dealing
+	# with POST data. This prevents data from being posted twice if a
+	# user hits the Back button.
+	return HttpResponseRedirect(reverse('VoteApp:results', args=(specific_question.id)))
