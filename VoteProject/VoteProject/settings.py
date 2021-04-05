81f2f466aad7e320c518895b1eeecaa0dceb78d9
pawroz <rozplochowskipawel9@gmail.com>
2021-04-05 14:01:16 +0200

    live postgres db on AWS

--- VoteProject/VoteProject/settings.py
+++ VoteProject/VoteProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'VoteProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'VoteApp',
+        'USER': ':):):)',
+        'PASSWORD':':):):)',
+        'HOST': 'database-1.crcyye2dz3yc.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
