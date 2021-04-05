dc5d52158fc59597f77bebff34c7672a8183850f
pawroz <rozplochowskipawel9@gmail.com>
2021-04-05 17:27:44 +0200

    settings - static files rename3

--- VoteProject/VoteProject/settings.py
+++ VoteProject/VoteProject/settings.py
@@ -126,9 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #SMTP Configuration
 
